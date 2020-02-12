e2afe85bf2dbd57c09b3ad5864d7c58f79fbfe60
Eloi Pardo <eloipr94@gmail.com>
2020-02-12 10:59:25 +0100

    windows changes

--- projects/cura-binary-data.cmake
+++ projects/cura-binary-data.cmake
@@ -1,7 +1,5 @@
-
 ExternalProject_Add(cura-binary-data
-    GIT_REPOSITORY https://github.com/ultimaker/cura-binary-data
-    GIT_TAG origin/${CURABINARYDATA_BRANCH_OR_TAG}
+    SOURCE_DIR C:/Users/epardo/Documents/Projects/Cura/cura-binary-data
     CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${EXTERNALPROJECT_INSTALL_PREFIX}
 )
 
