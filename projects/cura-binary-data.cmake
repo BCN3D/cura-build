b9a4e5da0ff20e50bfa0d044f33e5620b2594e4d
Pardo Rivero <epardo@bcn3d.local>
2020-02-19 16:10:20 +0100

    use binary-data repo

--- projects/cura-binary-data.cmake
+++ projects/cura-binary-data.cmake
@@ -1,5 +1,7 @@
+
 ExternalProject_Add(cura-binary-data
-    SOURCE_DIR C:/Users/epardo/Documents/Projects/Cura/cura-binary-data
+    GIT_REPOSITORY https://github.com/ultimaker/cura-binary-data
+    GIT_TAG origin/${CURABINARYDATA_BRANCH_OR_TAG}
     CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${EXTERNALPROJECT_INSTALL_PREFIX}
 )
 
